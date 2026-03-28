08da9a2cd1780e542607c6c1243cf3bc1f54dd87
Hugo <humellad@unirioja.es>
2026-03-28 12:31:18 +0100

    Fin

--- src/main/java/servicios/EjemploEmails.java
+++ src/main/java/servicios/EjemploEmails.java
@@ -1,8 +1,5 @@
 package servicios;
 
-import org.openapitools.client.*;
-import org.openapitools.client.api.*;
-import org.openapitools.client.model.*;
 import org.slf4j.Logger;
 
 import modelo.Destinatario;
@@ -16,24 +13,7 @@ public class EjemploEmails implements interfaces.InterfazEnviarEmails{
 	
 	@Override
 	public boolean enviarEmail(Destinatario dest, String email) {
-		// 1. Configurar el cliente (URL base de la API)
-        ApiClient apiClient = new ApiClient();
-        apiClient.setBasePath("https://api.tu-servidor.com");
-
-        // 2. Instanciar el servicio específico
-        EmailApi emailApi = new EmailApi(apiClient);
-
-        try {
-            // 3. Llamar al método (los parámetros coinciden con tu swagger.json)
-            EmailResponse respuesta = emailApi.emailPost("usuario@correo.com", "Hola desde Java");
-            
-            if (respuesta.getDone()) {
-                System.out.println("Correo enviado con éxito");
-            }
-        } catch (Exception e) {
-            // Manejo de errores de conexión o 400 Bad Request
-            System.err.println("Error al enviar email: " + e.getMessage());
-        }
+		
         return true;
 	}
 
